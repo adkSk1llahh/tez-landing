f00526675121c239fd4ecffe893a028e75c132fc
Адильхан Жумабеков <azhumabekov@wooppay.kz>
2021-09-28 10:35:41 +0600

    first com

---
+++ build/js/scripts.js
@@ -0,0 +1,57 @@
+$(".faq__button").on("click", function () {
+  if ($(this).next().hasClass("active")) {
+    $(this).next().removeClass("active");
+  } else {
+    $(".faq__text").removeClass("active");
+    $(this).next().addClass("active");
+  }
+});
+
+$(".contact-button").click(function() {
+    $(".modal--ask-question").toggleClass("active");
+    $(".overlay").toggleClass("visible");
+});
+
+$(".modal__button-close").click(function() {
+    $(".modal--ask-question").toggleClass("active");
+    $(".overlay").toggleClass("visible");
+});
+
+$(".header__button").click(function() {
+    $(".modal--menu").toggleClass("active");
+    $(".overlay").toggleClass("visible");
+});
+
+$(".menu-mobile__button").click(function() {
+    $(".modal--menu").toggleClass("active");
+    $(".overlay").toggleClass("visible");
+});
+
+
+
+
+$("#form").validate({
+  rules: {
+    user_name: {
+      required: true,
+    },
+    user_email: {
+      required: true,
+      email: true,
+    },
+    user_message: {
+      required: true,
+    },
+  },
+  messages: {
+    user_name: {
+      required: "Не допустимые символы",
+    },
+    user_email: {
+      required: "Не правильный адрес",
+    },
+    user_message: {
+      required: "Заполните форму",
+    },
+  },
+});
